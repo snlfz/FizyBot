4b3351b542976f8a4fcb07ef2105aa200ab26edd
snlfz <42074408+snlfz@users.noreply.github.com>
2018-08-07 16:36:59 +0530

    Update bot3.py

--- bot3.py
+++ bot3.py
@@ -31,7 +31,7 @@ from discord.ext.commands import Bot
 
 BOT_PREFIX = (",","$","<@472615266103328778> ","<@472615266103328778>")
 
-TOKEN =os.environ.get(Token) 
+TOKEN =os.environ.get('Token') 
 
 
 COLOUR=0x0
